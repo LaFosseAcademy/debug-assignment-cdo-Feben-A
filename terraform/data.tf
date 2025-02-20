b547be23ed623b21366ccd430c34c81f5e95e170
Feben-A <afa508165@gmail.com>
2025-02-20 10:29:00 +0000

    Edit aws_subnets local name

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,9 +2,9 @@ resource "aws_default_vpc" "default" {
 
 }
 
-data "aws_subnets" "default_" {
-    filter {
-        name = "vpc-id"
-        values = [aws_default_vpc.default.id]
-    }
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
 }
